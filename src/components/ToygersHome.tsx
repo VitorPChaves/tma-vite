f71e1fa4c6d540ec47ffd2ab90e4dd7b3e49361d
Vitor Patricio Chaves <vitor.patricio.chaves@gmail.com>
2024-01-29 20:54:27 -0300

    finishes home page

--- src/components/ToygersHome.tsx
+++ src/components/ToygersHome.tsx
@@ -17,7 +17,8 @@ export function ToygersHome() {
   return (
     <div className="">
       <TonConnectButton />
-      <div className="flex flex-col items-center justify-center w-full h-[200px] border-[2px] rounded-[20px] border-[#2EA7F2]">
+
+      <div className="relative flex flex-col items-center justify-center w-full h-[200px] border-[2px] rounded-[20px] border-[#2EA7F2] hover:scale-[1.02] smooth-transition">
         <p className="text-[18px] text-[#00000060]">
           $<b className="text-[#000000]">TOY</b>
         </p>
@@ -26,6 +27,58 @@ export function ToygersHome() {
           src="/toy-coin.png"
           className="mt-[-10px] w-[150px] h-[150px]"
         />
+        <div className="flex items-center justify-center absolute right-[0px] bottom-[-2px] mr-[14px] w-[100px] h-[34px] border-[2px] rounded-[10px] border-[#2EA7F2] bg-[#FFFFFF] cursor-pointer hover:scale-[1.08] smooth-transition">
+          <p className="text-center text-[#2EA7F2]">Buy</p>
+        </div>
+      </div>
+
+      <div className="mt-[20px] relative flex flex-col items-center justify-center w-full h-[200px] border-[2px] rounded-[20px] border-[#2EA7F2] hover:scale-[1.02] smooth-transition">
+        <b className="text-[18px] text-[#000000]">ToyBuilder</b>
+        <img
+          alt="toy"
+          src="/toybuilder.png"
+          className="mt-[-10px] w-[150px] h-[150px]"
+        />
+        <div className="flex items-center justify-center absolute right-[0px] bottom-[-2px] mr-[14px] w-[100px] h-[34px] border-[2px] rounded-[10px] border-[#2EA7F2] bg-[#FFFFFF] cursor-pointer hover:scale-[1.08] smooth-transition">
+          <p className="text-center text-[#2EA7F2]">Soon</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row gap-[4px] items-center justify-center w-full mt-[20px] mb-[10px]">
+        <b className="text-[#000000] text-[22px]">TOYGERS</b>
+        <img alt="locked" src="/padlock.png" className="w-[20px] h-[20px]" />
+      </div>
+
+      <div className="flex flex-row justify-around w-full gap-[20px]">
+        <div className="flex flex-col items-center justify-between w-[160px] md:w-[40%] h-[240px] border-[2px] rounded-[20px] border-[#2EA7F2] hover:scale-[1.02] smooth-transition">
+          <div className="flex flex-row w-full items-start justify-between p-[12px] text-[#000000] text-[12px] md:text-[14px]">
+            <p>Serie 1</p>
+            <p>100 units</p>
+          </div>
+          <img
+            alt="toy"
+            src="/toygers-gold.png"
+            className="h-[150px] mt-[-20px] hover:scale-[1.12] smooth-transition"
+          />
+          <div className="flex items-center justify-center mb-[-2px] w-[100px] h-[34px] border-[2px] rounded-[10px] border-[#2EA7F2] bg-[#FFFFFF] cursor-pointer hover:scale-[1.08] smooth-transition">
+            <p className="text-center text-[#2EA7F2]">Soon</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-between w-[160px] md:w-[40%] h-[240px] border-[2px] rounded-[20px] border-[#2EA7F2] hover:scale-[1.02] smooth-transition">
+          <div className="flex flex-row w-full items-start justify-between p-[12px] text-[#000000] text-[12px] md:text-[14px]">
+            <p>Serie 1</p>
+            <p>100 units</p>
+          </div>
+          <img
+            alt="toy"
+            src="/toygers-silver.png"
+            className="h-[150px] mt-[-20px] hover:scale-[1.12] smooth-transition"
+          />
+          <div className="flex items-center justify-center mb-[-2px] w-[100px] h-[34px] border-[2px] rounded-[10px] border-[#2EA7F2] bg-[#FFFFFF] cursor-pointer hover:scale-[1.08] smooth-transition">
+            <p className="text-center text-[#2EA7F2]">Soon</p>
+          </div>
+        </div>
       </div>
     </div>
   );
